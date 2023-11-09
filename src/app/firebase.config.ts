7e838ad49f7d83c1f74a296aef42e45682cbbe72
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-11-09 04:30:00 +0400

    finishUpdate

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuKYd_o03u3eaw1Bk0aiVIXUAfGkDY1JI",
+  authDomain: "giorgi-mindiashvili.firebaseapp.com",
+  projectId: "giorgi-mindiashvili",
+  storageBucket: "giorgi-mindiashvili.appspot.com",
+  messagingSenderId: "485137267822",
+  appId: "1:485137267822:web:990379aa46f01149527a7f"
+};
